f07cd0f9ed8c0ab4b053c9594c097003c2a4fdd1
logicalguess <logicalguess@gmail.com>
2017-09-23 11:57:08 -0400

    small refactoring

--- src/main/scala/pf/loops/Iterator.scala
+++ src/main/scala/pf/loops/Iterator.scala
@@ -1,7 +1,7 @@
 package pf.loops
 
 case class Iterator[A](f: PartialFunction[A, A]) {
-  def apply(): PartialFunction[A, A]  = {
+  private def apply(): PartialFunction[A, A]  = {
     case a if f.isDefinedAt(a) =>
       var state = a
       while (f.isDefinedAt(state))
